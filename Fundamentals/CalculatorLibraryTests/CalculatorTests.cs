adaf7dbcfeccc531a4fa4c7e58734b9e1b46d415
Baran Daşdemir <76569348+barandasdemir0@users.noreply.github.com>
2026-05-26 16:52:57 +0300

    Fundamentals kısmı bitirilerek arrange act assert kısımları öğrendi

---
+++ Fundamentals/CalculatorLibraryTests/CalculatorTests.cs
@@ -0,0 +1,80 @@
+﻿using CalculatorLibrary;
+using Xunit.Abstractions;
+
+namespace CalculatorLibraryTests;
+
+public class CalculatorTests:IAsyncLifetime /*IDisposable*/ /*--> bu normal yapılar için asenkron için ise=>  IAsyncLifetime kullanılır*/
+{
+    private readonly Calculator _sut = new();
+    private readonly Guid _guid = Guid.NewGuid();
+
+    private readonly ITestOutputHelper _outputHelper;
+
+    public CalculatorTests(ITestOutputHelper outputHelper)
+    {
+        _outputHelper = outputHelper;
+        _outputHelper.WriteLine("Hello From the ctor");
+    }
+
+
+    [Theory/*(Skip ="örnek doğrultusunda facta değilde thoerye yazılır")*/]
+    [InlineData(5,4,9)]
+    [InlineData(0,0,0,Skip="tek biri çalıştırılmak istenmezse ise böyle yazılır")]
+    [InlineData(-5,-5,-10)]
+    //bu üstteki çağırılmalar bir methodu farklı farklı çağırarak her testi rahatça ve kod kalabalığı olmadan yapmamızı sağlar
+    /*[Fact]*///test olduğuunu belirtmek adına bu önemli bunu yazmazsak test explorerda gözükmez 
+    public void Add_ShouldAddTwoNumbers_WhenTwoNumbersAreIntegers(int a,int b,int expected)
+    {
+        //arrange kısmı : classların set edildiği ve newlendiği kısımdır --> duruma göre bu kısım kaldırılabilir yukarıya alınır
+        //var calculator = new Calculator();
+
+        //act metotların çağırıldığı ve çalıştırıldığı ve sonuçların yakalandığı
+        //var result = calculator.Add(5, 4);
+        var result = _sut.Add(a, b);
+        _outputHelper.WriteLine("Hello From the add method");
+
+        //istediğimiz sonucu burası takip ediyor sonuçların yakalandığı sonuçların kontrol edildiği kısımdır
+        Assert.Equal(expected, result);
+    }
+
+    [Fact]
+    public void Subtract_ShouldSubtractTwoNumbers_WhenTwoNumbersAreIntegers()
+    {
+        var result = _sut.Subtract(10,7);
+
+        _outputHelper.WriteLine("subtract metodu çalıştı");
+
+        Assert.Equal(3, result);
+    }
+
+    //public void Dispose()
+    //{
+    //    _outputHelper.WriteLine("Hello From the cleanup");
+    //}
+
+    [Fact(Skip ="Bu testler geçersiz")]
+    public void TestGuid1()
+    {
+        _outputHelper.WriteLine(_guid.ToString());
+        _outputHelper.WriteLine("Hello From the guid 1 metot");
+    }
+
+    [Fact(Skip = "Bu testler geçersiz")]
+    public void TestGuid2()
+    {
+        _outputHelper.WriteLine(_guid.ToString());
+        _outputHelper.WriteLine("Hello From the guid 2 metot");
+    }
+
+    public async  Task InitializeAsync()
+    {
+        _outputHelper.WriteLine("Hello From the initialize");
+        Task.Delay(1);
+    }
+
+    public async Task DisposeAsync()
+    {
+        _outputHelper.WriteLine("Hello From the dispose async");
+        Task.Delay(2);
+    }
+}
---
+++ Fundamentals/CalculatorLibraryTests/HomeClass.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CalculatorLibrary.Tests.Unit;
+
+public class HomeClass
+{
+    private readonly Calculator _calculator = new();
+
+
+    [Theory]
+    [InlineData(5,5,10)]
+    [InlineData(-5,5,0)]
+    [InlineData(-15,-5,-20)]
+    public void Added_ShouldAddTwoNumbers_WhenTwoNumbersAreIntegers(int a ,int b,int expected)
+    {
+        var result = _calculator.Add(a, b);
+        Assert.Equal(expected, result);
+    }
+
+    [Theory]
+    [InlineData(5,-5,10)]
+    [InlineData(15,-5,20)]
+    [InlineData(-5,-5,0)]
+    [InlineData(-15,-5,-10)]
+    [InlineData(5,-10,15)]
+    public void Subtract_ShouldSubtractTwoNumbers_WhenTwoNumbersAreIntegers(int a ,int b,int expected)
+    {
+        var result = _calculator.Subtract(a, b);
+        Assert.Equal(expected, result);
+    }
+
+    [Theory]
+    [InlineData(5, 5, 25)]
+    [InlineData(50, 0, 0)]
+    [InlineData(-5, 5, -25)]
+    public void Multiply_ShouldMultiplyTwoNumbers_WhenTwoNumbersAreIntegers(int a, int b, int expected)
+    {
+        var result = _calculator.Multiply(a, b);
+        Assert.Equal(expected, result);
+    }
+
+    [Theory]
+    [InlineData(5, 5, 1)]
+    [InlineData(15, 5, 3)]
+    [InlineData(0, 0, Skip ="Bu Test ignore edilmiştir")]
+    public void Divide_ShouldDivideTwoNumbers_WhenTwoNumbersAreIntegers(int a, int b, int expected)
+    {
+        var result = _calculator.Divide(a, b);
+        Assert.Equal(expected, result);
+    }
+
+
+}
